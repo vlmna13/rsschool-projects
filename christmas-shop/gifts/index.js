921f227039842f81bcdbd56177ffa12464eacc7c
vlmna13 <vlmna13@gmail.com>
2024-11-26 19:39:50 +0300

    fix: fix bugs with burger menu

--- christmas-shop/gifts/index.js
+++ christmas-shop/gifts/index.js
@@ -1,19 +1,29 @@
 const navContainer = document.querySelector('.nav__container');
 const bodyNoScroll = document.querySelector('body');
 const burgerButton = document.querySelector('.burger__menu');
+const lineFirst = document.querySelector('.burger__line_first');
+const lineSecond = document.querySelector('.burger__line_second');
 
 function toggleBurgerMenu() {
+    const scrollTop = window.scrollY;
+    navContainer.style.top = 64 - scrollTop + 'px';
+
+    navContainer.style.height = `calc(100vh - ${navContainer.style.top})`;
+    console.log(`calc(100vh - ${navContainer.style.top})`);
+
     navContainer.classList.toggle('open');
     bodyNoScroll.classList.toggle('open');
     burgerButton.classList.toggle('open');
-    const scrollTop = window.scrollY;
-    navContainer.style.top = 64 - scrollTop + 'px';
+    lineFirst.classList.toggle('open');
+    lineSecond.classList.toggle('open');
 }
 
 function closeBurgerMenu() {
     burgerButton.classList.remove('open');
     navContainer.classList.remove('open');
     bodyNoScroll.classList.remove('open');
+    lineFirst.classList.remove('open');
+    lineSecond.classList.remove('open');
 }
 
 burgerButton.addEventListener('click', toggleBurgerMenu);
@@ -23,6 +33,8 @@ document.querySelectorAll('.nav__item').forEach( (item) => {
 })
 
 document.querySelector('.logo__container').addEventListener('click', closeBurgerMenu);
+
+
 
 
 
