9e2a4a81fa1de1adb0e6b95702344d4a73d19dc1
vlmna13 <vlmna13@gmail.com>
2024-11-26 17:35:44 +0300

    feat: add action for burger menu

---
+++ christmas-shop/gifts/index.js
@@ -0,0 +1,28 @@
+const navContainer = document.querySelector('.nav__container');
+const bodyNoScroll = document.querySelector('body');
+const burgerButton = document.querySelector('.burger__menu');
+
+function toggleBurgerMenu() {
+    navContainer.classList.toggle('open');
+    bodyNoScroll.classList.toggle('open');
+    burgerButton.classList.toggle('open');
+    const scrollTop = window.scrollY;
+    navContainer.style.top = 64 - scrollTop + 'px';
+}
+
+function closeBurgerMenu() {
+    burgerButton.classList.remove('open');
+    navContainer.classList.remove('open');
+    bodyNoScroll.classList.remove('open');
+}
+
+burgerButton.addEventListener('click', toggleBurgerMenu);
+
+document.querySelectorAll('.nav__item').forEach( (item) => {
+    item.addEventListener('click', closeBurgerMenu);
+})
+
+document.querySelector('.logo__container').addEventListener('click', closeBurgerMenu);
+
+
+
