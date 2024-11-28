a0ed7726a1c60534b8569aba4d4dbbc25112d4ba
vlmna13 <vlmna13@gmail.com>
2024-11-28 13:24:33 +0300

    feat: add action for slider

--- christmas-shop/home/index.js
+++ christmas-shop/home/index.js
@@ -35,3 +35,81 @@ document.querySelectorAll('.nav__item').forEach( (item) => {
 document.querySelector('.logo-container').addEventListener('click', closeBurgerMenu);
 
 //SLIDER
+
+const allSlides = document.querySelectorAll('.slider__item');
+const sliderWrapper = document.querySelector('.slider__wrapper');
+const sliderTrack = document.querySelector('.slider__track');
+const buttonNext = document.querySelector('.right__button');
+const buttonPrev = document.querySelector('.left__button');
+let offset = 0;
+let click;
+let count = 0;
+let chunk;
+let width;
+
+function init() {
+    width = sliderTrack.offsetWidth;
+    offset = 0;
+    sliderTrack.style.left = 0;
+    click = 0;
+    count = 0;
+    buttonPrev.classList.add('disabled');
+    buttonPrev.setAttribute('disabled', "");
+    if(sliderWrapper.offsetWidth > 768) {
+        click = 3;
+    } else {
+        click = 6;    
+        console.log('else')
+    }
+    chunk = (width - sliderWrapper.offsetWidth) / click;
+    console.log(width)
+    console.log(click)
+    console.log(count)
+}
+init();
+
+window.addEventListener('resize', init)
+
+buttonNext.addEventListener('click', moveRight);
+
+function moveRight() {
+    count = count + 1;
+    if(count === click){
+        offset = offset - chunk;
+        sliderTrack.style.left = offset + 'px';
+        buttonNext.classList.add('disabled');
+        buttonNext.setAttribute("disabled", "");
+        count = click;
+    } else {
+        buttonPrev.classList.remove('disabled');
+        buttonPrev.removeAttribute('disabled');
+        offset = offset - chunk;
+        sliderTrack.style.left = offset + 'px';
+    }
+    console.log('right')
+}
+
+function moveLeft() {
+    console.log(count)
+    count = count - 1;
+    if(count === 0){
+        offset = offset + chunk;
+        sliderTrack.style.left = offset + 'px';
+        buttonPrev.classList.add('disabled');
+        buttonPrev.setAttribute("disabled", "");
+        count = 0;
+    } else {
+        buttonNext.classList.remove('disabled');
+        buttonNext.removeAttribute('disabled');
+        offset = offset + chunk;
+        sliderTrack.style.left = offset + 'px';
+    }
+    console.log('left')
+}
+
+buttonPrev.addEventListener('click', moveLeft);
+
+
+
+
+
