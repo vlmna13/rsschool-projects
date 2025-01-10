f395e3c44f6c262d284d907168db07d0170756f2
vlmna13 <vlmna13@gmail.com>
2025-01-11 00:52:14 +0300

    fix: fix function connection

--- simon-says/components/creators/connection.js
+++ simon-says/components/creators/connection.js
@@ -1,14 +1,21 @@
 import { defaultParameters } from "../../index.js";
+import { checkInput } from "../checkInput.js";
 
 export function connection(event){
     const key = document.querySelector(`[data-index=${event.code}]`);
     if(!key) {
         return;
     }
-    key.classList.add(defaultParameters.level)
+    key.classList.add('active');
+
+    console.log(defaultParameters.level)
     const field = document.querySelector('.field');
-    field.innerText += event.key; 
+    // field.innerText += event.key; 
+    const symbolWrapper = document.createElement('span');
+    symbolWrapper.innerText = key.innerText;
+    field.appendChild(symbolWrapper);
     setTimeout(() => {
-        key.classList.add(defaultParameters.level);
+        key.classList.remove('active');
     }, 400);
+    checkInput();
 }
