8c8c9460f8485c16959f5612e315f65e4bf32d20
vlmna13 <vlmna13@gmail.com>
2025-01-12 11:48:30 +0300

    fix: update checkInput

--- simon-says/components/checkInput.js
+++ simon-says/components/checkInput.js
@@ -1,24 +1,55 @@
 import { defaultParameters } from "../index.js";
 import { keyboardDataLetters, keyboardDataNumbers } from "./keyboardData.js";
 import { allowInput } from "./allowInput.js";
+import { repeatSeq } from "./repeatSeq.js";
+import { connection } from "./creators/connection.js";
+import { createLevel } from "./creators/creatLevel.js";
 
 export function checkInput(key) {
     let value = key;
     const field = document.querySelector('.field');
+    const userInput = field.innerText.split('');
     const allkeys = document.querySelectorAll('.key');
-    let seq = Array.from(defaultParameters.sequence);
     let data = defaultParameters.data;
-    if(value == data[seq[0]].inside) {
-        seq.shift();
-    } else {
-        console.log('mistake');
-        field.textContent = '';
-        defaultParameters.mistakes -= 1;
-        if(defaultParameters.mistakes === 0){
-            alert('defeat')
+    let seq = defaultParameters.sequence;
+    const feedBack = document.querySelector('.feed-back');
+    const repeat = document.querySelector('.repeat');
+    const start = document.querySelector('.start');
+    const arr = [];
+    seq.forEach(el => {
+        arr.push(data[el].inside)
+    })
+    for (let index = 0; index < userInput.length; index++) {
+        if (userInput[index] !== data[seq[index]].inside) {
+            feedBack.innerText = 'first mistake';
+            field.innerText = '';
+            defaultParameters.mistakes -= 1;
+            if (defaultParameters.mistakes === 0) {
+                repeat.style.color = 'gray';
+                repeat.removeEventListener('click', repeatSeq);
+                defaultParameters.isGamestarted = false;
+                feedBack.innerText = '';
+                feedBack.innerText = 'game over';
+                allowInput();
+                start.disabled = false;
+            }
+            return;
         }
     }
-
-
+    if (userInput.length === seq.length && userInput.every((element, index) => element === arr[index])) {
+        feedBack.innerText = 'you win this level';
+        allkeys.forEach(el => {
+            document.removeEventListener('keydown', connection);
+            el.disabled = true;
+        });
+        repeat.removeEventListener('click', repeatSeq);
+        repeat.innerText = 'next level';
+        if (!repeat.classList.contains('next-level')) {
+            defaultParameters.round += 1;
+            repeat.classList.add('next-level');
+        }
+        repeat.addEventListener('click', createLevel);
+    }
+   
 }
 
