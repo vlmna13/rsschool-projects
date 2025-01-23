89c8d576d56c8ca8db1b08c395d55ad4320be300
vlmna13 <vlmna13@gmail.com>
2025-01-23 23:53:20 +0300

    feat: add sound for clicks

--- nonograms/src/main/rightClick.js
+++ nonograms/src/main/rightClick.js
@@ -2,13 +2,18 @@ import { nonogramsList } from "../nonogramsList.js";
 import { checkWin } from "./checkWin.js";
 
 export function rightClick(event, matrix) {
+    let audioCross = new Audio('../sounds/cross.mp3');
     if (event.target.classList.contains('colored')) {
         event.target.classList.remove('colored');
         event.target.textContent = 'X';
+        audioCross.play();
     } else if(event.target.textContent == 'X'){
         event.target.textContent = '';
+        let audioWhite = new Audio('../sounds/white.mp3');
+        audioWhite.play();
     } else {
         event.target.textContent = 'X';
+        audioCross.play();
     }
     checkWin(matrix);
 }
--- nonograms/src/main/userClick.js
+++ nonograms/src/main/userClick.js
@@ -2,12 +2,16 @@ import { nonogramsList } from "../nonogramsList.js";
 import { checkWin } from "./checkWin.js";
 
 export function userClick(event, matrix ) {
+    let audioColored = new Audio('../sounds/colored.mp3');
+    let audioWhite = new Audio('../sounds/white.mp3');
     if(event.target.classList.contains('colored')) {
         event.target.classList.remove('colored');
         event.target.textContent = '';
+        audioWhite.play();
     } else {
         event.target.classList.add('colored');
         event.target.textContent = '';
+        audioColored.play();
     }
     checkWin(matrix);
 }
