69611fbe7eba452a9248b9d722b1e074e643fb7f
vlmna13 <vlmna13@gmail.com>
2025-01-26 18:48:57 +0300

    fix: fix checkWin

--- nonograms/src/main/checkWin.js
+++ nonograms/src/main/checkWin.js
@@ -3,28 +3,26 @@ import { nonogramsList } from "../nonogramsList.js";
 import { fillTable } from "./fillTable.js";
 
 export function checkWin(matrix) {
-    // console.log('passs  ' +matrix)
-    // let userMatrix = Array.from({ length: matrix.length }, () => Array(matrix.length).fill(0));
-    // settings.userMatrix = userMatrix;
+    let userMatrix = Array.from({ length: matrix.length }, () => Array(matrix.length).fill(0));
+    settings.userMatrix = userMatrix;
     // const ceilsWrapper = document.querySelector('.nonogram-wrapper');
-    // const allCeils = document.querySelectorAll('.colored');
-    // allCeils.forEach(el => {
-    //     userMatrix[el.dataset.row][el.dataset.col] = 1;
-    // });
+    const allCeils = document.querySelectorAll('.colored');
+    allCeils.forEach(el => {
+        userMatrix[el.dataset.row][el.dataset.col] = 1;
+    });
+    // let userMatrixFlat = userMatrix.flat();
+
     let userMatrixFlat = settings.userMatrix.flat();
     let matrixFlat = matrix.flat();
-    userMatrixFlat.forEach(el => {
+    userMatrixFlat.forEach((el, index) => {
         if(el === 'X'){
-            el = 0;
+            userMatrixFlat[index] = 0;
         }
     });
-
     if(userMatrixFlat.every((element, index) => element === matrixFlat[index])) {
-        console.log(settings.isGameStarted, settings.intervalId)
+        // console.log('checkWin before false '+settings.isGameStarted, settings.intervalId)
         settings.isGameStarted = false;
         settings.intervalId = clearInterval(settings.intervalId);
-        console.log(settings.isGameStarted, settings.intervalId)
-
         let gameInfo = [settings.level, settings.variant, settings.minutes, settings.seconds];
         let winTable = JSON.parse(localStorage.getItem('winTable')) || [];
         if(winTable.length >= 5) {
@@ -33,5 +31,7 @@ export function checkWin(matrix) {
         winTable.push(gameInfo);
         localStorage.setItem('winTable', JSON.stringify(winTable));
         fillTable();
+
     } 
+
 }
