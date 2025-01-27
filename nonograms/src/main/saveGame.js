79bda0eeecb71a2715609faa532dd92055c32997
vlmna13 <vlmna13@gmail.com>
2025-01-27 21:15:36 +0300

    fix: user cant save game if its not started

--- nonograms/src/main/saveGame.js
+++ nonograms/src/main/saveGame.js
@@ -2,7 +2,8 @@ import { nonogramsList } from "../nonogramsList.js";
 import { settings } from "../../index.js";
 
 export function saveGame() {
-    if(!settings.blockedCeil) {
+    if(!settings.blockedCeil || !settings.isGameStarted) {
+        console.log(JSON.parse(localStorage.getItem('settings')))
         return;
     }
     localStorage.setItem('settings', JSON.stringify(settings));
