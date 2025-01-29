6abaeb3c359fb594b4c90dcc77cc151d313d34c0
vlmna13 <vlmna13@gmail.com>
2025-01-29 21:35:59 +0300

    fix: timer doest work if user switch on the other page

--- nonograms/src/main/playPauseTimer.js
+++ nonograms/src/main/playPauseTimer.js
@@ -2,11 +2,9 @@ import { settings } from "../../index.js";
 
 export function playPauseTimer() {
     const timerWrapper = document.querySelector('.timer-wrapper');
-    if(settings.isGameStarted == false){
-        settings.isGameStarted = true;
+    function startTimer() {
         settings.intervalId = setInterval(() => {
             settings.seconds = settings.seconds + 1;
-            // timerWrapper.textContent = 'Time: '+ settings.minutes + ':' + settings.seconds;
             if(settings.seconds === 59){
                 settings.minutes = settings.minutes + 1;
                 settings.seconds = 0;
@@ -14,7 +12,24 @@ export function playPauseTimer() {
             const formattedMinutes = settings.minutes < 10 ? '0' + settings.minutes : settings.minutes;
             const formattedSeconds = settings.seconds < 10 ? '0' + settings.seconds : settings.seconds;
             timerWrapper.textContent = formattedMinutes + ' : ' + formattedSeconds;
-        }, 1000)
+        }, 1000);
     }
+
+    if(settings.isGameStarted == false){
+        settings.isGameStarted = true;
+        startTimer();
+    }
+
+    document.addEventListener('visibilitychange', () => {
+        if (document.hidden) {
+            clearInterval(settings.intervalId);
+            settings.isGameStarted = false;
+        } else {
+            if (!settings.isGameStarted) {
+                settings.isGameStarted = true;
+                startTimer();
+            }
+        }
+    });
 
 }
