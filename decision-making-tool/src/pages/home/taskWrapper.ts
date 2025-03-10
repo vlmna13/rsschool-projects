296d5245b5328385ba62a46ad2ba8d3ee25d693f
vlmna13 <vlmna13@gmail.com>
2025-03-10 21:06:48 +0300

    refactor: update initState and taskWrapper

--- decision-making-tool/src/pages/home/taskWrapper.ts
+++ decision-making-tool/src/pages/home/taskWrapper.ts
@@ -1,10 +1,47 @@
 import { createElement } from '../../utils/createElement';
 import './home.css';
+import { createLabel } from '../../components/label';
+import { createInput, choice } from '../../components/input';
+import { createButtonDelete } from './buttonsInteraction';
 
-export function createTaskWrapper(): HTMLLIElement {
+export interface TaskWrapperOptions {
+  id: string;
+  title: string;
+  weight: string;
+}
+
+export function createTaskWrapper(options: TaskWrapperOptions): HTMLLIElement {
   const taskWrapper = createElement<HTMLLIElement>({
     tag: 'li',
     classNames: ['task-wrapper'],
   });
+
+  const label = createLabel({
+    textContent: options.id,
+    htmlFor: `task-input-${options.id}`,
+  });
+
+  const inputTitle = createInput({
+    id: `task-input-${options.id}`,
+    value: options.title,
+    placeholder: choice.title,
+    name: choice.title,
+  });
+
+  const inputWeight = createInput({
+    id: `weight-input-${options.id}`,
+    value: options.weight,
+    placeholder: choice.weight,
+    name: choice.weight,
+    type: 'number',
+  });
+
+  const buttonDelete = createButtonDelete();
+
+  taskWrapper.appendChild(label);
+  taskWrapper.appendChild(inputTitle);
+  taskWrapper.appendChild(inputWeight);
+  taskWrapper.appendChild(buttonDelete);
+
   return taskWrapper;
 }
---
+++ decision-making-tool/src/utils/functionInit.ts
@@ -0,0 +1,23 @@
+export interface DecisionState {
+  optionsList: {
+    list: { id: string; title: string; weight: string }[];
+    lastId: number;
+  };
+}
+
+export function initState(): DecisionState {
+  const defaultState: DecisionState = {
+    optionsList: {
+      list: [{ id: '#1', title: '', weight: '' }],
+      lastId: 1,
+    },
+  };
+
+  const storedState = localStorage.getItem('decisionState');
+  if (!storedState) {
+    localStorage.setItem('decisionState', JSON.stringify(defaultState));
+    return defaultState;
+  } else {
+    return JSON.parse(storedState);
+  }
+}
