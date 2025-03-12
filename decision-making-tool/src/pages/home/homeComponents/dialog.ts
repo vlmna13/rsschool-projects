cdb3b6a081b3c6cfa79ae49b3a40384d82b2b99b
vlmna13 <vlmna13@gmail.com>
2025-03-12 20:56:32 +0300

    feat: create dialog

---
+++ decision-making-tool/src/pages/home/homeComponents/dialog.ts
@@ -0,0 +1,47 @@
+import { createElement } from '../../../utils/createElement';
+import '../home.css';
+import '../../../components/componentsStyles.css';
+import { createButtonClose, createButtonConfirm } from './buttonsDialog';
+
+export function createDialog(): HTMLDialogElement {
+  const dialog = createElement<HTMLDialogElement>({
+    tag: 'dialog',
+    classNames: ['dialog-wrapper'],
+  });
+
+  const form = createElement<HTMLFormElement>({
+    tag: 'form',
+    classNames: ['form-element'],
+  });
+  dialog.appendChild(form);
+
+  const textareaElement = createElement<HTMLTextAreaElement>({
+    tag: 'textarea',
+    classNames: ['textarea-element'],
+  });
+  textareaElement.placeholder = `Paste a list of new options in a CSV-like format:
+
+  title,1                 -> | title                 | 1 |
+  title with whitespace,2 -> | title with whitespace | 2 |
+  title , with , commas,3 -> | title , with , commas | 3 |
+  title with "quotes",4   -> | title with "quotes"   | 4 |`;
+  textareaElement.rows = 12;
+  textareaElement.cols = 64;
+  form.appendChild(textareaElement);
+
+  const buttonCancel = createButtonClose();
+
+  form.appendChild(buttonCancel);
+  document.body.style.overflow = 'hidden';
+  buttonCancel.addEventListener('click', () => {
+    dialog.close();
+    document.body.style.overflow = 'auto';
+  });
+  const buttonConfirm = createButtonConfirm();
+  form.appendChild(buttonConfirm);
+  document.body.appendChild(dialog);
+  dialog.showModal();
+  document.body.style.overflow = 'hidden';
+
+  return dialog;
+}
