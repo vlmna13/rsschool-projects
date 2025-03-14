e3344c560e9fcab280b9e882fe8ade98123bf612
vlmna13 <vlmna13@gmail.com>
2025-03-14 15:05:50 +0300

    feat: add function validateStart

---
+++ decision-making-tool/src/pages/home/functionValidateStart.ts
@@ -0,0 +1,18 @@
+import { DecisionState } from '../../utils/functionInit';
+import { router } from '../../utils/router';
+import { showMessage } from './functionShowMessage';
+
+export function validateStart() {
+  const stringData = localStorage.getItem('decisionState');
+  if (!stringData) {
+    return;
+  }
+  const data: DecisionState = JSON.parse(stringData);
+  const values = Object.values(data.optionsList.list);
+  const validItems = values.filter((item) => item.title && item.weight);
+  if (validItems.length >= 2) {
+    router.navigate('wheel');
+  } else {
+    showMessage();
+  }
+}
--- decision-making-tool/src/pages/home/functionValidateUserInput.ts
+++ decision-making-tool/src/pages/home/functionValidateUserInput.ts
@@ -1,10 +1,12 @@
 import { DecisionState } from '../../utils/functionInit';
 
 export function validateUserInput(value: string) {
+  if (value.length === 0) {
+    return '';
+  }
   const itemsList = value.split('\n');
   const storedState = localStorage.getItem('decisionState');
   let defaultState: DecisionState;
-
   if (storedState) {
     defaultState = JSON.parse(storedState);
   } else {
@@ -19,18 +21,14 @@ export function validateUserInput(value: string) {
     let lastComa = item.lastIndexOf(',');
     let title = item.substring(0, lastComa).trim();
     let weight = item.substring(lastComa + 1).trim();
-
     if (!title) {
       title = '';
     }
-
     if (!weight || isNaN(Number(weight))) {
       weight = '';
     }
-
     const newId = `${defaultState.optionsList.lastId + 1}`;
     defaultState.optionsList.lastId += 1;
-
     return {
       id: newId,
       title: title,
@@ -41,6 +39,5 @@ export function validateUserInput(value: string) {
     defaultState.optionsList.list[item.id] = item;
   });
   localStorage.setItem('decisionState', JSON.stringify(defaultState));
-
   return newItems;
 }
