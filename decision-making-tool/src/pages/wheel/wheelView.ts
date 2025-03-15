5ca57d17e9b473ffc8cf1de55e9d71595586b5c1
vlmna13 <vlmna13@gmail.com>
2025-03-15 20:22:37 +0300

    feat: add wheel page and styles

--- decision-making-tool/src/pages/wheel/wheelView.ts
+++ decision-making-tool/src/pages/wheel/wheelView.ts
@@ -1,7 +1,18 @@
+import './wheelView.css';
+import { createElement } from '../../utils/createElement';
+import { createControlWrapper } from './functionCreateControlWrapper';
+
 export function wheelView(main: HTMLElement) {
   while (main.children.length > 1) {
     main.removeChild(main.lastChild!);
   }
   main.classList.remove('main-home-container');
   main.className = 'main-wheel-container';
+  const contentWrapper = createElement({
+    tag: 'div',
+    classNames: ['content-wrapper'],
+  });
+  main.appendChild(contentWrapper);
+  const controlWrapper = createControlWrapper();
+  contentWrapper.append(controlWrapper);
 }
