33619545f478e334a13be2f20efb0fbf67442477
vlmna13 <vlmna13@gmail.com>
2025-03-18 00:03:13 +0300

    feat: fixed deployment config

--- decision-making-tool/vite.config.ts
+++ decision-making-tool/vite.config.ts
@@ -1,12 +1,5 @@
 import { defineConfig } from 'vite';
-import { ghPages } from "vite-plugin-gh-pages";
 
 export default defineConfig({
   base: '/vlmna13-JSFE2024Q4/decision-making-tool/',
-  plugins: [
-    ghPages({
-      branch: 'gh-pages',
-      dest: 'decision-making-tool'
-    })
-  ],
 });
