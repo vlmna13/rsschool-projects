0beb55e32fa5e2ecbc256bf27a6fa397a71308fc
vlmna13 <vlmna13@gmail.com>
2025-03-27 22:14:03 +0300

    feat: add cardata file

---
+++ async-race/public/data/carsData.ts
@@ -0,0 +1,28 @@
+type CarData = {
+  [key: string]: string[];
+};
+
+const carData: CarData = {
+  bmw: ["x3", "x4", "x5", "m2", "m3"],
+  toyota: ["corolla", "camry", "highlander, landcruiser", "hilux"],
+  peugeot: ["308", "206", "408", "traveller", "rifter"],
+  mitsubishi: ["lancer", "outlander", "pajero", "colt", "galant"],
+  ford: ["focus", "mondeo", "puma", "kuga", "explorer"],
+  chevrolet: ["bolt", "onix", "malibu", "camaro", "corvette"],
+  volvo: ["s60", "s90", "v60", "v90", "xc40"],
+  skoda: ["fabia", "scala", "octavia", "superb", "kodiaq"],
+  opel: ["zafira", "vivaro", "corsa", "astra", "mokka"],
+  lada: ["granta", "vesta", "aura", "largus", "niva legend"],
+  hyundai: ["elantra", "ioniq", "sonata", "graundeur", "casper"],
+  fiat: ["panda", "grande panda", "tipo", "500x", "600"],
+  audi: ["a1", "a3", "s3", "rs3", "e-tron gt"],
+  volkswagen: ["polo", "golf", "passat", "arteon", "taigo"],
+  renault: ["clio", "sandero", "duster", "captur", "megan"],
+  nissan: ["note", "versa", "juke", "murano", "patrol"],
+  mazda: ["cx-4", "cx-30", "cx-60", "mx-5", "ez-6"],
+  kia: ["ceed", "rio", "sportage", "carnival", "sorento"],
+  honda: ["accord", "civic", "element", "logo", "pilot"],
+  citroen: ["space-tourer", "jumpy", "c5-x", "aircross", "basalt"],
+};
+
+export { carData };
