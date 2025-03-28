a05dac3629a89c2d7181bb52f814425395ac9ea1
vlmna13 <vlmna13@gmail.com>
2025-03-28 14:04:31 +0300

    feat: add createElement and some types for data

---
+++ async-race/src/utils/createElement.ts
@@ -0,0 +1,22 @@
+export interface ElementOptions<T extends HTMLElement> {
+  tag: string;
+  classNames: string[];
+  textContent: string;
+}
+
+export function createElement<T extends HTMLElement>(
+  options: ElementOptions<T>,
+): T {
+  const element = document.createElement(options.tag) as T;
+  if (options.classNames) {
+    options.classNames.forEach((className) => {
+      element.classList.add(className);
+    });
+  }
+
+  if (options.textContent) {
+    element.textContent = options.textContent;
+  }
+
+  return element;
+}
---
+++ async-race/src/utils/types.ts
@@ -0,0 +1,7 @@
+export type Car = {
+  name: string;
+  color: string;
+  id: number;
+};
+
+export type GarageResponse = Car[];
