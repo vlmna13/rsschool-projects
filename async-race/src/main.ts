00aa9ac6e3d196ad77a998c34661181da64909ec
vlmna13 <vlmna13@gmail.com>
2025-03-31 00:19:48 +0300

    refactor: update main.ts

---
+++ async-race/src/main.ts
@@ -0,0 +1,23 @@
+import { GarageView } from "./pages/garage/garageView";
+import { winnersView } from "./pages/winners/winnersView";
+import { router } from "./utils/router";
+import "./styles/common.css";
+import { createMainElement } from "./pages/components/mainElement";
+// import { createWrapperButtons } from "./pages/components/buttonsRouting/buttonsRouting";
+
+const { mainElement, childView } = createMainElement();
+const garageView = new GarageView(childView);
+document.addEventListener("DOMContentLoaded", () => {
+  router.addRoute("garage", () => garageView.render());
+
+  router.addRoute("winners", () => winnersView(childView));
+  router.setErrorComponent(() => console.log("Page not found"));
+
+  document.body.append(mainElement);
+
+  if (!location.hash) {
+    router.navigate("garage"); // Переходим на страницу garage при загрузке, если хэш пустой
+  } else {
+    router.loadRoute(location.hash); // Загружаем текущий маршрут
+  }
+});
--- async-race/src/pages/components/mainElement.ts
+++ async-race/src/pages/components/mainElement.ts
@@ -1,10 +1,21 @@
 import { createElement } from "../../utils/createElement";
 import "../../styles/common.css";
+import { createWrapperButtons } from "./buttonsRouting/buttonsRouting";
 
 export function createMainElement() {
   const mainElement = createElement<HTMLElement>({
     tag: "main",
     classNames: ["main"],
   });
-  return mainElement;
+
+  const wrapperButtons = createWrapperButtons();
+
+  const childView = createElement<HTMLDivElement>({
+    tag: "div",
+    classNames: ["child-view"],
+  });
+
+  mainElement.append(wrapperButtons, childView);
+
+  return { mainElement, childView };
 }
