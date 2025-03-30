3bd7c15736ba1674404fa2c3fd447bde5c62855f
vlmna13 <vlmna13@gmail.com>
2025-03-31 00:16:26 +0300

    refactor: update buttonsRouting

--- async-race/src/pages/components/buttonsRouting/buttonsRouting.ts
+++ async-race/src/pages/components/buttonsRouting/buttonsRouting.ts
@@ -1,31 +1,26 @@
 import { createElement } from "../../../utils/createElement";
 import "./buttonsRouting.css";
 
-export function createButtonGarage() {
-  let buttonGarage = createElement<HTMLButtonElement>({
-    tag: "button",
-    classNames: ["button", "button-garage"],
-    textContent: "TO GARAGE",
-  });
-  return buttonGarage;
-}
-
-export function createButtonWinners() {
-  let buttonWinners = createElement<HTMLButtonElement>({
-    tag: "button",
-    classNames: ["button", "button-winners"],
-    textContent: "TO WINNERS",
-  });
-  return buttonWinners;
-}
-
 export function createWrapperButtons() {
-  let wrapperButtons = createElement<HTMLDivElement>({
+  const wrapperButtons = createElement<HTMLDivElement>({
     tag: "div",
     classNames: ["wrapper-buttons-routing"],
   });
-  const buttonGarage = createButtonGarage();
-  const buttonWinners = createButtonWinners();
+
+  const buttonGarage = createElement<HTMLLinkElement>({
+    tag: "a",
+    classNames: ["button", "button-garage"],
+    textContent: "TO GARAGE",
+  });
+  buttonGarage.href = "#garage";
+
+  const buttonWinners = createElement<HTMLLinkElement>({
+    tag: "a",
+    classNames: ["button", "button-winners"],
+    textContent: "TO WINNERS",
+  });
+  buttonWinners.href = "#winners";
+
   wrapperButtons.append(buttonGarage, buttonWinners);
   return wrapperButtons;
 }
