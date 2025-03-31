e3d91e5507b5e0cf7e4a72d2fe4c5d1436fb6292
vlmna13 <vlmna13@gmail.com>
2025-03-31 11:52:46 +0300

    refactor: rewraight on the class formEditCar

--- async-race/src/pages/garage/formCreateEditCar/formEditCar.ts
+++ async-race/src/pages/garage/formCreateEditCar/formEditCar.ts
@@ -1,30 +1,69 @@
 import { createElement } from "../../../utils/createElement";
 import "./formCreateEditCar.css";
 
-export function createFormEditCar() {
-  const formWrapper = createElement<HTMLDivElement>({
-    tag: "div",
-    classNames: ["form-edit-car"],
-  });
-  const inputText = createElement<HTMLInputElement>({
-    tag: "input",
-    classNames: ["input-text", "disabled"],
-  });
-  inputText.disabled = true;
-  inputText.type = "text";
-  const inputColor = createElement<HTMLInputElement>({
-    tag: "input",
-    classNames: ["input-color", "disabled"],
-  });
-  inputColor.type = "color";
-  inputColor.disabled = true;
-  const buttonEdit = createElement<HTMLButtonElement>({
-    tag: "button",
-    classNames: ["button", "button-edit", "disabled"],
-    textContent: "UPDATE",
-  });
-  buttonEdit.disabled = true;
-  formWrapper.append(inputText, inputColor, buttonEdit);
+// export function createFormEditCar() {
+//   const formWrapper = createElement<HTMLDivElement>({
+//     tag: "div",
+//     classNames: ["form-edit-car"],
+//   });
+//   const inputText = createElement<HTMLInputElement>({
+//     tag: "input",
+//     classNames: ["input-text", "disabled"],
+//   });
+//   inputText.disabled = true;
+//   inputText.type = "text";
+//   const inputColor = createElement<HTMLInputElement>({
+//     tag: "input",
+//     classNames: ["input-color", "disabled"],
+//   });
+//   inputColor.type = "color";
+//   inputColor.disabled = true;
+//   const buttonEdit = createElement<HTMLButtonElement>({
+//     tag: "button",
+//     classNames: ["button", "button-edit", "disabled"],
+//     textContent: "UPDATE",
+//   });
+//   buttonEdit.disabled = true;
+//   formWrapper.append(inputText, inputColor, buttonEdit);
 
-  return formWrapper;
+//   return formWrapper;
+// }
+
+export class FormEditCar {
+  private formWrapper: HTMLDivElement;
+  private inputText: HTMLInputElement;
+  private inputColor: HTMLInputElement;
+  private buttonEdit: HTMLButtonElement;
+  constructor(private garageContainer: HTMLDivElement) {
+    this.formWrapper = createElement<HTMLDivElement>({
+      tag: "div",
+      classNames: ["form-edit-car"],
+    });
+
+    this.inputText = createElement<HTMLInputElement>({
+      tag: "input",
+      classNames: ["input-text", "disabled"],
+    });
+    this.inputText.disabled = true;
+    this.inputText.type = "text";
+
+    this.inputColor = createElement<HTMLInputElement>({
+      tag: "input",
+      classNames: ["input-color", "disabled"],
+    });
+    this.inputColor.type = "color";
+    this.inputColor.disabled = true;
+
+    this.buttonEdit = createElement<HTMLButtonElement>({
+      tag: "button",
+      classNames: ["button", "button-edit", "disabled"],
+      textContent: "UPDATE",
+    });
+    this.buttonEdit.disabled = true;
+
+    this.formWrapper.append(this.inputText, this.inputColor, this.buttonEdit);
+  }
+  public render(): HTMLDivElement {
+    return this.formWrapper;
+  }
 }
