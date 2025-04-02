fc6e104642414947f82eb0412c129c5960f387ca
vlmna13 <vlmna13@gmail.com>
2025-04-02 14:36:34 +0300

    refactor: rewright moveCarResponse

---
+++ async-race/src/pages/garage/raceField/functionMoveCarResponse.ts
@@ -0,0 +1,39 @@
+import { Move } from "../../../utils/types";
+export enum MoveErrorCode {
+  WRONG_PARAMETERS = "WRONG_PARAMETERS",
+  NOT_FOUND = "NOT_FOUND",
+  TOO_MANY_REQUESTS = "TOO_MANY_REQUESTS",
+  ENGINE_BROKEN = "ENGINE_BROKEN",
+  NETWORK_ERROR = "NETWORK_ERROR",
+  UNKNOWN_ERROR = "UNKNOWN_ERROR",
+}
+
+export async function moveCarResponse(id: number) {
+  const url = `http://127.0.0.1:3000/engine?id=${id}&status=drive`;
+  try {
+    const response = await fetch(url, {
+      method: "PATCH",
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP ${response.status}`);
+    }
+    const data: Move = await response.json();
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      switch (error.message) {
+        case "HTTP 400":
+          return { code: MoveErrorCode.WRONG_PARAMETERS };
+        case "HTTP 404":
+          return { code: MoveErrorCode.NOT_FOUND };
+        case "HTTP 429":
+          return { code: MoveErrorCode.TOO_MANY_REQUESTS };
+        case "HTTP 500":
+          return { code: MoveErrorCode.ENGINE_BROKEN };
+        default:
+          return { code: MoveErrorCode.UNKNOWN_ERROR };
+      }
+    }
+    return { error: "An unknown error occurred." };
+  }
+}
