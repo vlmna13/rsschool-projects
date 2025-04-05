0229d9864282dc3995df6806693444fc6fc0f5a2
vlmna13 <vlmna13@gmail.com>
2025-04-05 16:29:10 +0300

    refactor: update determoneWinner for correct compare the result

--- async-race/src/pages/garage/raceControlbuttons/determineWinner.ts
+++ async-race/src/pages/garage/raceControlbuttons/determineWinner.ts
@@ -1,26 +1,29 @@
 import { getWinnerResponse } from "./functionGetWinnerResponse";
 import { createWinnerResponse } from "./functionCreateWinnerResponse";
 import { updateWinnerResponse } from "./functionUpdateWinnerResponce";
+import { showWinner } from "./functionShowWinner";
 
-export async function determineWinner(id: number, time: number): Promise<void> {
+export async function determineWinner(
+  id: number,
+  time: number,
+): Promise<HTMLDivElement | void> {
   try {
     const winner = await getWinnerResponse(id);
     const newWins = winner.wins + 1;
-    const roundedTime = Math.round(time * 100) / 100;
+    const roundedTime = Math.round((time / 1000) * 100) / 100;
     const newTime = roundedTime < winner.time ? roundedTime : winner.time;
 
     await updateWinnerResponse(id, newWins, newTime);
+    await showWinner(id, roundedTime);
   } catch (error) {
     if (error instanceof Error && error.message.includes("not found")) {
       // Если победитель не найден, создаём нового
-      await createWinnerResponse(id, 1, time);
-      console.log(
-        `Победитель создан: ID ${id}, Победы: 1, Лучшее время: ${time}`,
-      );
+      const roundedTime = Math.round((time / 1000) * 100) / 100;
+      await createWinnerResponse(id, 1, roundedTime);
+      await showWinner(id, roundedTime);
     } else {
       // Обрабатываем другие ошибки
-      console.error("Ошибка при обработке победителя:", error);
-      throw error; // Пробрасываем ошибку для обработки на уровне вызова
+      console.error("Ошибка при обработке победителя:");
     }
   }
 }
