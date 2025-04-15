455c78e1c834214e039d6187412d2e4666c26b55
vlmna13 <vlmna13@gmail.com>
2025-04-15 14:32:10 +0300

    feat: add more rules

--- fun-chat/eslint.config.js
+++ fun-chat/eslint.config.js
@@ -24,12 +24,35 @@ export default [
     },
     linterOptions: {
       noInlineConfig: true,
+      reportUnusedDisableDirectives: "error",
     },
     rules: {
       "max-lines-per-function": [
         "error",
         { max: 40, skipComments: true, skipBlankLines: true },
       ],
+      "@typescript-eslint/no-magic-numbers": [
+        "error",
+        {
+          ignore: [0, 1],
+          ignoreEnums: true,
+          ignoreNumericLiteralTypes: true,
+          ignoreReadonlyClassProperties: true,
+          enforceConst: true,
+        },
+      ],
+      "@typescript-eslint/consistent-type-assertions": [
+        "error",
+        { assertionStyle: "never" },
+      ],
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/explicit-member-accessibility": [
+        "error",
+        { accessibility: "explicit", overrides: { constructors: "off" } },
+      ],
+      "@typescript-eslint/member-ordering": "error",
+      "class-methods-use-this": "error",
     },
   },
 ];
