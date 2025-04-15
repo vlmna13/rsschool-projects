8acf56910d65b69c86a68811f24501cb43034d2f
vlmna13 <vlmna13@gmail.com>
2025-04-15 23:45:45 +0300

    update: update styles

--- fun-chat/src/footer/footerComponent.ts
+++ fun-chat/src/footer/footerComponent.ts
@@ -1,28 +1,27 @@
-import './footerComponent.css';
-import { Component } from '../utils/component';
+import "./footerComponent.css";
+import { Component } from "../utils/component";
 
-
-export class FooterComponent extends Component<'footer'> {
+export class FooterComponent extends Component<"footer"> {
   constructor() {
     super({
-      tag: 'footer',
-      className: 'footer-wrapper',
+      tag: "footer",
+      className: "footer-wrapper",
     });
     const author = new Component({
-      tag: 'a',
-      className: 'author-link',
-      text: 'vlmna13'
+      tag: "a",
+      className: "author-link",
+      text: "vlmna13",
     });
-    author.getNode().setAttribute('href', 'https://github.com/vlmna13');
-    author.getNode().setAttribute('target', '_blank');
+    author.getNode().setAttribute("href", "https://github.com/vlmna13");
+    author.getNode().setAttribute("target", "_blank");
 
     const rsschool = new Component({
-        tag: 'a',
-        className: 'rsschool-link',
-        text: 'RSSchool'
+      tag: "a",
+      className: "rsschool-link",
+      text: "RSSchool",
     });
-    rsschool.getNode().setAttribute('href', 'https://rs.school/');
-    rsschool.getNode().setAttribute('target', '_blank');
+    rsschool.getNode().setAttribute("href", "https://rs.school/");
+    rsschool.getNode().setAttribute("target", "_blank");
     this.appendChildren([author, rsschool]);
   }
 }
