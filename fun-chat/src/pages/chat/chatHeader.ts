b79b8ecd64517895aca1ccc201c72407ddf98c4c
vlmna13 <vlmna13@gmail.com>
2025-04-18 13:37:21 +0300

    feat: add function exit

--- fun-chat/src/pages/chat/chatHeader.ts
+++ fun-chat/src/pages/chat/chatHeader.ts
@@ -1,37 +1,71 @@
 import "./chatView.css";
 import { Component } from "../../utils/component";
 import { InfoButtonComponent } from "../login/infobuttonComponent";
+import type { WebSocketManager } from "../../utils/webSocketManager";
+import type { UserLogoutRequest } from "../../utils/requestTypes";
+
+type User = {
+  login: string;
+  password: string;
+  isLogined: boolean;
+};
 
 export class ChatHeader extends Component<"div"> {
-  constructor(userLogin: string, mainComponent: Component<"main">) {
+  private wsManager: WebSocketManager;
+  private user: User;
+  constructor(
+    user: User,
+    mainComponent: Component<"main">,
+    wsManager: WebSocketManager,
+  ) {
     super({
       tag: "div",
       className: "chat-header",
     });
-
+    this.wsManager = wsManager;
+    this.user = user;
     const circle = new Component({
       tag: "div",
       className: "circle",
-      text: userLogin[0].toUpperCase(),
+      text: user.login[0].toUpperCase(),
     });
     const chatTitle = new Component({
       tag: "h3",
       className: "chat-title",
-      text: userLogin,
+      text: user.login,
     });
 
     const buttonExit = new Component({
       tag: "button",
-      className: "button-exit",
-      text: "Выйти",
+      className: "exit-button",
+      text: "Перестать веселиться",
+    });
+    buttonExit.addListener("click", () => {
+      this.exit();
     });
     const buttonWrapper = new Component({
       tag: "div",
-      className: "button-wrapper",
+      className: "buttons-wrapper",
     });
     const infoButton = new InfoButtonComponent(mainComponent);
     buttonWrapper.appendChildren([buttonExit, infoButton]);
     this.appendChildren([circle, chatTitle, buttonWrapper]);
-    console.log("chatheader  ", userLogin);
+  }
+
+  private async exit(): Promise<void> {
+    try {
+      const logoutRequest: UserLogoutRequest = {
+        user: {
+          login: this.user.login,
+          password: this.user.password,
+        },
+      };
+      await this.wsManager.sendRequest("USER_LOGOUT", logoutRequest);
+      console.log("User logged out:", this.user.login);
+    } catch (error) {
+      console.error("Failed to notify server about logout:", error);
+    }
+    sessionStorage.removeItem("user");
+    location.reload();
   }
 }
